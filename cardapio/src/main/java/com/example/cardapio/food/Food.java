5354198aec06964a76e14b5dff3223c058c3ca85
Wendy Miller <weendymilleer@gmail.com>
2023-11-17 19:38:16 -0300

    backend da aplicação

---
+++ cardapio/src/main/java/com/example/cardapio/food/Food.java
@@ -0,0 +1,45 @@
+package com.example.cardapio.food;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "food")
+@Entity(name = "food")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")//indica que o id é representação unica do food
+public class Food {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String image;
+    private Integer price;
+
+    public Food(FoodRequestDTO data){
+         this.image = data.image();
+         this.price = data.price();
+         this.title = data.title();
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+}
